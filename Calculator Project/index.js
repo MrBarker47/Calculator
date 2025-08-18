07ba89f39bc67f6791efbcfee4c2873216d36a86
MrBarker47 <jebarker7@gmail.com>
2025-08-18 03:04:02 -0400

    Update files

--- Calculator Project/index.js
+++ Calculator Project/index.js
@@ -20,9 +20,13 @@ function multiply(a,b) {
  return a * b;
 }
 
-//Divide function
+
 function divide(a,b) {
-  return a / b;
+    if(b === 0) {
+      return "Error: Division by zero"
+    } else {
+      return a / b
+    }
 }
 
 //Operator
@@ -31,7 +35,7 @@ function operate(a,b,operator) {
     return add(a,b);
   } else if (operator === "-") {
     return subtract(a,b);
-  } else if (operator === "*") {
+  } else if (operator === "*" || operator === 'x') {
     return multiply(a,b) 
   } else if (operator === "/") {
     return divide(a,b) 
@@ -44,9 +48,11 @@ function operate(a,b,operator) {
 //  Display
 function display() {
   btn.forEach(button => {
-    button.addEventListener('click', () =>{
+    button.addEventListener('click', (e) =>{
         let numbers = document.getElementById('text')
-        numbers.value += button.innerText;
+         if(button.id !== 'equal' && button.id !== 'clear') {
+          numbers.value += button.innerText;
+         }
     })
   })
 }
@@ -58,20 +64,12 @@ function clear() {
  })
 }
 
-function divide(a,b) {
-    if(b === 0) {
-      return "Error: Division by zero"
-    } else {
-      return a / b
-    }
-}
 
 //Equals
 function solve() {
   newEqual.addEventListener("click", (e) => {
     let numbers = document.getElementById('text');
-    // let newEqual = document.getElementById('equal').value = "";
-    const match = numbers.value.match(/^(\d+)([+\-*/])(\d+)$/);
+    const match = numbers.value.match(/^(-?\d+\.?\d*)([+\-\/\**])(-?\d+\.?\d*)$/);
     if (match) {
       let a =  Number(match[1]);
       let operator = match[2];
